802579f8d68234665aa78fac5ad42806d23a3dcf
bghanw <bghanw@bbcag.ch>
2021-01-13 14:23:56 +0100

    intial commit

---
+++ src/main/java/ch/bbcag/hellostudent/HelloStudent.java
@@ -0,0 +1,14 @@
+package ch.bbcag.hellostudent;
+
+public class HelloStudent {
+
+    public static void main(String[] args) {
+
+        //for (int i = 0; i < 9; i++){
+
+            //System.out.println("This is a test Print");
+        //}
+
+
+    }
+}
---
+++ src/main/java/ch/bbcag/hellostudent/syntaxauftrag/Auftrag_02.java
@@ -0,0 +1,11 @@
+package ch.bbcag.hellostudent.syntaxauftrag;
+
+public class Auftrag_02 {
+
+    String test;
+
+    public static void main(String[] args) {
+        test = "Hello";
+    }
+
+}
