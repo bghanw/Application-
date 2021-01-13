c2a2ad6a4d62e71ffc6175092f90e1b8faf9a367
bghanw <bghanw@bbcag.ch>
2021-01-13 16:44:22 +0100

    changes in aufgabe3

--- src/main/java/ch/bbcag/hellostudent/syntaxauftrag_02/Aufgabe3.java
+++ src/main/java/ch/bbcag/hellostudent/syntaxauftrag_02/Aufgabe3.java
@@ -1,5 +1,18 @@
 package ch.bbcag.hellostudent.syntaxauftrag_02;
 
 public class Aufgabe3 {
-    
+
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i < 6 ; i++){
+
+            for (int j = 0 ; j < i ; j++){
+
+                System.out.print ("*");
+                System.out.print (" ");
+            }
+            System.out.println("o");
+        }
+
+    }
 }
